4a278a0d283f326faac1516b95e042800fafa5ae
Corinto <salazar72390@gmail.com>
2023-03-30 18:03:08 +0800

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,5 @@
+	function kiloweightConvert(value) {
+	document.getElementById("Kilogram").innerHTML=value*2.2046;
+	document.getElementById("Ounces").innerHTML=value*16;
+	document.getElementById("Grams").innerHTML=value*0.0022046;
+	}
